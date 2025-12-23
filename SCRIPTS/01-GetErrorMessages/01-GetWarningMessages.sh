effa671dd64617920d5d3326333e35608bd08dd3
Rochak Wadhwa <rochak.wadhwa@gmail.com>
2025-12-23 09:03:20 +0000

    Updated recent changes

---
+++ SCRIPTS/01-GetErrorMessages/01-GetWarningMessages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###############################################################################
+# Script Name   : 
+# Description   : This script fetches error messages
+# Author        : Rochak Wadhwa
+# Created Date  : 2025-03-15
+# Last Modified : 2025-03-15
+# Version       : 1.0
+###############################################################################
+
+
+grep -i warning /workspaces/MyLinuxMachine/SCRIPTS/01-GetErrorMessages/application.log
